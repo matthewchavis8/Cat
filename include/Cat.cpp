602b534db7a595b9bc0d8ff65d65a55e8d88465b
matthewchavis8 <matthewchavis8@gmail.com>
2025-02-24 10:04:57 -0600

    Breaking up different parses into different lambdas

--- include/Cat.cpp
+++ include/Cat.cpp
@@ -11,18 +11,25 @@ void Cat::parse_file (const std::string& path) {
     std::string line{};
     std::ostringstream res;
 
+    //Parse with line numbers
+    auto parse_file_with_line_numbers = [&]() {
+        while (std::getline(file, line)) {
+            res << line_count << ": " << line << '\n';
+                line_count++;
+        }
+    };
+    // Regular parse 
+    auto parse_file_with_default = [&]() {
+        while (std::getline(file, line)) 
+            res << line << '\n';
+    };
+
     if (file.is_open()) {
         
-        if (is_line_numbers) {
-            while (std::getline(file, line)) {
-                res << line_count << ": " << line << '\n';
-                line_count++;
-            }
-        } else {
-            while (std::getline(file, line)) {
-                res << line << '\n';
-            }
-        }
+        if (is_line_numbers) 
+            parse_file_with_line_numbers();
+        else 
+            parse_file_with_default();
         
     } else {
         std::cerr << "Error parsing our file" << '\n';
